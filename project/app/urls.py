099afdb04c6bbda8a416b63e81cc8fe5b919b42e
mehak <mehakpatelattitude03@gmail.com>
2024-08-05 18:20:23 +0530

    class based api

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [
-    path('stu_list/',stulist,name='stu'),
-    path('stu_detail/<int:pk>',studetail,name='stud')  
-    # path('list/',list,name='list')
+# urlpatterns = [
+#     path('stu_list/',stulist,name='stu'),
+#     path('stu_detail/<int:pk>',studetail,name='stud')  
+#     # path('list/',list,name='list')
+# ]
+
+
+# -------------------------class based 
+urlpatterns=[
+    path('stulist/', Stulist.as_view()),
+    path('studetail/<int:pk>/',Studetail.as_view())
+    
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -9,6 +9,8 @@ import io
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status 
+from rest_framework.views import APIView
+from django.http import Http404
 
 
 
@@ -161,47 +163,87 @@ from rest_framework import status
 
    
 # --------------------------function based api-----------------------------
-@api_view(['GET', 'POST']) 
-def stulist(request):
-    if request.method=='GET':
-        user=StudentModel.objects.all()
-        serializer=StudentSerializer(user,many=True)
-        return Response(serializer.data)
+# @api_view(['GET', 'POST']) 
+# def stulist(request):
+#     if request.method=='GET':
+#         user=StudentModel.objects.all()
+#         serializer=StudentSerializer(user,many=True)
+#         return Response(serializer.data)
     
-    elif request.method=='POST':
-        serializer=StudentSerializer(data=request.data) 
-        if serializer.is_valid(): 
-            serializer.save() 
-            return Response(serializer.data) 
-        else: return Response(serializer.errors)
+#     elif request.method=='POST':
+#         serializer=StudentSerializer(data=request.data) 
+#         if serializer.is_valid(): 
+#             serializer.save() 
+#             return Response(serializer.data) 
+#         else: return Response(serializer.errors)
 
-@api_view(['GET','PUT','DELETE'])
-def studetail(request,pk):
-    user=StudentModel.objects.filter(id=pk)
-    if user:
-        if request.method=='GET':
-            users=StudentModel.objects.get(id=pk) 
-            serializer = StudentSerializer(users) 
-            return Response(serializer.data) 
+# @api_view(['GET','PUT','DELETE'])
+# def studetail(request,pk):
+#     user=StudentModel.objects.filter(id=pk)
+#     if user:
+#         if request.method=='GET':
+#             users=StudentModel.objects.get(id=pk) 
+#             serializer = StudentSerializer(users) 
+#             return Response(serializer.data) 
         
-        elif request.method=='PUT': 
-            user=StudentModel.objects.get(id=pk) 
-            serializer = StudentSerializer(user,data=request.data,partial=True) 
-            if serializer.is_valid(): 
-                serializer.save() 
-                return Response(serializer.data) 
-            else: return Response(serializer.errors)
+#         elif request.method=='PUT': 
+#             user=StudentModel.objects.get(id=pk) 
+#             serializer = StudentSerializer(user,data=request.data,partial=True) 
+#             if serializer.is_valid(): 
+#                 serializer.save() 
+#                 return Response(serializer.data) 
+#             else: return Response(serializer.errors)
 
-        elif request.method=='DELETE': 
-                user=StudentModel.objects.get(id=pk) 
-                user.delete() 
-                return Response({'msg':"Data deleted successfully"}, 
-                status=status.HTTP_204_NO_CONTENT)
+#         elif request.method=='DELETE': 
+#                 user=StudentModel.objects.get(id=pk) 
+#                 user.delete() 
+#                 return Response({'msg':"Data deleted successfully"}, 
+#                 status=status.HTTP_204_NO_CONTENT)
     
-    res = {'msg': 'id not present in Database'}
-    return Response({'msg': 'id not present in Database'})
-    
+#     res = {'msg': 'id not present in Database'}
+#     return Response({'msg': 'id not present in Database'})
 
+#------------------------------------------------------CLASS BASED API-----------------------------------
+class Stulist(APIView):
+    def get(self,request):
+        user=StudentModel.objects.all()
+        serializers=StudentSerializer(user,many=True)
+        return Response(serializers.data)
+    
+    def post(self,request):
+        serializers=StudentSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Studetail(APIView):
+    def get_object(self,pk):
+        try:
+            return StudentModel.objects.get(id=pk)
+        except StudentModel.DoesNotExist:
+            raise Http404       
+        
+    def get(self,request,pk):
+        user=self.get_object(pk)
+        serializers=StudentSerializer(user,data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return Response(serializers.errors ,status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self,request,pk):
+        user=self.get_object(pk)
+        serializers=StudentSerializer(user,partial=True)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        return  Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,request,pk):
+        user=self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 
